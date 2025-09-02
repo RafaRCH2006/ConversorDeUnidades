81e82d81ee62290fb30df5dcacc9f3d55db13289
RafaRCH2006 <crooxel40@gmail.com>
2025-09-02 16:02:22 -0600

    Creacion de carpetas y alguna libreria para sabe rsi funciona

---
+++ setup.py
@@ -0,0 +1,9 @@
+##Menu del programa
+
+def menu():
+    print("Seleccione una opción:")
+    print("1. Opción 1")
+    print("2. Opción 2")
+    print("3. Salir")
+    opcion = input("Ingrese el número de la opción deseada: ")
+    return opcion
